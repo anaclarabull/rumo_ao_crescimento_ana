d7f72802f68e5e123c91a0a5259698c5ada57331
Ana Clara Mendes Bull <89466535+anaclarabull@users.noreply.github.com>
2021-12-20 22:24:58 -0300

    login

--- src/components/Logar/index.tsx
+++ src/components/Logar/index.tsx
@@ -1,11 +1,14 @@
 import './styles.css';
-
+import { ReactComponent as LogImage } from 'assets/images/logomedio.svg';
 import { Link } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 
 const Logar = () => {
     return (
+        
         <div className="login-container">
+            <div className="base-card login" >
+            
             <div className="user-login">
                 <h1 className="user-login__title">Acessar o Sistema</h1>
                 <form autoComplete="nope">
@@ -17,13 +20,19 @@ const Logar = () => {
                         <label htmlFor="password">Senha</label>
                         <input id="password" type="password" name="password" />
                     </div>
-                    <div className="button ">
+                    <div className="btn-container">
                         <Link to="/Admin">
                             <ButtonIcon text="Entrar"/>
                         </Link>
+                        <div className="nav-logo-container">
+        <Link to="/" className="nav-logo-text">
+              <LogImage />
+            </Link>
+            </div>
                     </div>
                 </form>
             </div>
+        </div>
         </div>
     );
 };
